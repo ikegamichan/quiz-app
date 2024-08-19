bb213b96f1a31619dc78dde2d871de0ee7514707
ikegamichan <ikegamitakuto@gmail.com>
2024-08-20 02:16:12 +0900

    クリックでroot-pathに戻るように変更

--- src/components/header/left-parts/index.tsx
+++ src/components/header/left-parts/index.tsx
@@ -1,3 +1,18 @@
+'use client';
+
+import { PATHS } from '@/const/paths';
+import { useRouter } from 'next/navigation';
+
 export const LeftParts = () => {
-  return <div className=" font-bold text-xl mx-6 text-nowrap">TOP</div>;
+  const { push } = useRouter();
+  return (
+    <div
+      className=" font-bold text-xl mx-6 text-nowrap cursor-pointer"
+      onClick={() => {
+        push(PATHS.ROOT);
+      }}
+    >
+      TOP
+    </div>
+  );
 };
